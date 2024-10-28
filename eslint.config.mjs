f1fab480a98f948a062f859f9ea168d4bbff4692
Manish Dahiya <manishdahiya0944@gmail.com>
2024-10-28 20:23:13 +0530

    add  logger

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,16 +4,16 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  ...tseslint.configs.recommended,
-  {
-    ignores: ["dist", "node_modules", "eslint.config.mjs"],
-  },
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    ...tseslint.configs.recommended,
+    {
+        ignores: ["dist", "node_modules", "eslint.config.mjs"],
     },
-  },
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
 );
