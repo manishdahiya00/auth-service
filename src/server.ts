488f332f9d6bab1b8602712cf84034808fd050b6
Manish Dahiya <manishdahiya0944@gmail.com>
2024-10-28 20:22:55 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 const startServer = () => {};
 try {
     const PORT = Config.PORT;
-    app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+    app.listen(PORT, () =>
+        logger.info("Server listening on port ", { port: PORT }),
+    );
 } catch (error) {
     console.log(error);
     process.exit(1);
