7fc35765e5bd8b18009dd3b924ae2b138c092723
Manish Dahiya <manishdahiya0944@gmail.com>
2024-10-28 20:35:14 +0530

    used error handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import createHttpError, { HttpError } from "http-errors";
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Welcome to Auth Service");
+app.get("/", async (req, res, next) => {
+    const err = createHttpError(401, "You cannot access this route.");
+    next(err);
 });
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        errors: [
+            { type: err.name, message: err.message, path: "", location: "" },
+        ],
+    });
+});
 export default app;
