83219f43b7ed2b903828bfdea2e21958100771fa
Manish Dahiya <manishdahiya0944@gmail.com>
2024-10-28 19:50:09 +0530

    add express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello");
+import app from "./app";
+import { Config } from "./config";
+const startServer = () => {};
+try {
+    const PORT = Config.PORT;
+    app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+} catch (error) {
+    console.log(error);
+    process.exit(1);
+}
+
+startServer();
