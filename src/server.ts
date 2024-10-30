be10e6f4781d84d25bbfcb707d037ddba4e95e44
Manish Dahiya <manishdahiya0944@gmail.com>
2024-10-30 22:08:28 +0530

    add db connection toserver

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
 import app from "./app";
 import { Config } from "./config";
+import { AppDataSource } from "./config/data-source";
 import logger from "./config/logger";
-const startServer = () => {};
-try {
+const startServer = async () => {
     const PORT = Config.PORT;
-    app.listen(PORT, () =>
-        logger.info("Server listening on port ", { port: PORT }),
-    );
-} catch (err) {
-    console.log(err);
-    process.exit(1);
-}
+
+    try {
+        await AppDataSource.initialize();
+        logger.info("Database connection established");
+        app.listen(PORT, () =>
+            logger.info("Server listening on port ", { port: PORT }),
+        );
+    } catch (err) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            process.exit(1);
+        }
+    }
+};
 
 startServer();
